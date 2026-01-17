38900fcfd1c6311c1742f693ff6019094c9472ca
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2026-01-17 22:10:00 +0300

    refactor: remove question, response, survey, and user modules and their related DTOs and entities

--- src/response/dto/update-response.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateResponseDto } from './create-response.dto';
-
-export class UpdateResponseDto extends PartialType(CreateResponseDto) {}
--- src/survey/dto/update-survey.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSurveyDto } from './create-survey.dto';
-
-export class UpdateSurveyDto extends PartialType(CreateSurveyDto) {}
